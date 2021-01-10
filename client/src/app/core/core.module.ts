ad287230ef202638b3ff8a87c4397dfc612ced77
Suisoku <dev.ziani@gmail.com>
2021-01-10 11:04:58 +0100

    product page added

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { ContentFeedComponent } from './components/content-feed/content-feed.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @NgModule({
-  declarations: [NavbarComponent, SidenavComponent, ContentFeedComponent],
+  declarations: [NavbarComponent, SidenavComponent, ContentFeedComponent, PageHeaderComponent],
   imports: [
     CommonModule,
     SharedModule
@@ -14,7 +15,9 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
   exports: [
     NavbarComponent,
     SidenavComponent,
-    ContentFeedComponent
+    ContentFeedComponent,
+    PageHeaderComponent
   ]
 })
 export class CoreModule { }
+  
