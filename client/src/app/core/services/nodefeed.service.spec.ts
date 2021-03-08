7832ee7f418e654e7b52b573bd38d232836a7e92
Suisoku <dev.ziani@gmail.com>
2021-03-08 13:10:25 +0100

    migration to angular 11

--- client/src/app/core/services/nodefeed.service.spec.ts
+++ client/src/app/core/services/nodefeed.service.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject, waitForAsync } from '@angular/core/testing';
 import { NodefeedService } from './nodefeed.service';
 
 describe('Service: Nodefeed', () => {
