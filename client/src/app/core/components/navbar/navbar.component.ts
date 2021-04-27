39984364517fe4f594263f0dc17f32923e1a710e
Noure <dev.ziani@gmail.com>
2021-04-27 23:18:55 +0200

    fixed eslint path issue

--- client/src/app/core/components/navbar/navbar.component.ts
+++ client/src/app/core/components/navbar/navbar.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthenticationService } from 'src/app/features/account/services/authentication.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  displayBurger = false;
+  isLoggedIn$: Observable<boolean>;
+
+  constructor(private auth: AuthenticationService) {
+    this.isLoggedIn$ = this.auth.isLoggedIn$;
+  }
+}
