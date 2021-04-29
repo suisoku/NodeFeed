233ba2985a54eb711feead977c7de5c106b1171a
Noure <dev.ziani@gmail.com>
2021-04-29 20:11:46 +0200

    1

--- client/src/app/features/account/account-routing.module.ts
+++ client/src/app/features/account/account-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
-import { AngularFireAuthGuard, AuthPipe } from '@angular/fire/auth-guard';
+import { AngularFireAuthGuard, AuthPipe, redirectLoggedInTo } from '@angular/fire/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
 import { pipe } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { SignPageComponent } from './pages/sign-page/sign-page.component';
 import { VerifyEmailPageComponent } from './pages/verify-email-page/verify-email-page.component';
 
@@ -14,16 +15,22 @@ const emailVerifiedRedirect = () =>
     map((emailNotVerified) => emailNotVerified || ['/'])
   );
 
+const redirectLoggedIn = () => redirectLoggedInTo(['/']);
+//I need a pipe if you are logged and email not verified
+
+
+//Control access at module level (and not at root level) to facilitate refactoring
 const routes: Routes = [
   { path: '', redirectTo: 'signin', pathMatch: 'full' },
-  { path: 'signin', component: SignPageComponent },
-  { path: 'signup', component: SignPageComponent },
+  { path: 'signin', component: SignPageComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectLoggedIn } },
+  { path: 'signup', component: SignPageComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectLoggedIn } },
   {
     path: 'signup/verify-email',
     component: VerifyEmailPageComponent,
     canActivate: [AngularFireAuthGuard],
     data: { authGuardPipe: emailVerifiedRedirect }
-  }
+  },
+  { path: 'profile', component: ProfilePageComponent }
 ];
 
 @NgModule({
