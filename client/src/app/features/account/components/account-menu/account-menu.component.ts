867a4636d2e907a23ab011c1922c6baf1dbf5ae2
Noureddine <dev.ziani@gmail.com>
2021-05-01 23:54:14 +0200

    account menu disable verify

--- client/src/app/features/account/components/account-menu/account-menu.component.ts
+++ client/src/app/features/account/components/account-menu/account-menu.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewChecked, AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, QueryList, ViewChildren } from '@angular/core';
 import { MatTooltip } from '@angular/material/tooltip';
 import { FirebaseUser } from 'src/firebase-app';
 import { AuthenticationService } from '../../services/authentication.service';
@@ -13,11 +13,13 @@ export class AccountMenuComponent implements AfterViewInit {
   user: FirebaseUser | null = null;
   isLoadingUser = true;
 
-  @ViewChild('verifyEmailTooltip') set verifyEmailTooltip(tooltip: MatTooltip) {
-    console.log('setted', tooltip);
-  }
+  get isEmailVerified(): boolean {
+    return this.user?.emailVerified ?? true;
+  };
 
-  constructor(private auth: AuthenticationService) {
+  @ViewChildren('verifyEmailTooltip') verifyEmailTooltip!: QueryList<MatTooltip>;
+
+  constructor(private auth: AuthenticationService, private cdr: ChangeDetectorRef) {
     this.auth.currentUser$.subscribe((user) => {
       this.user = user;
       this.isLoadingUser = false;
@@ -25,20 +27,13 @@ export class AccountMenuComponent implements AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    console.log('checked', this.verifyEmailTooltip);
-    //this.verifyEmailTooltip.show();
-    //setInterval(() => this.verifyEmailTooltip.hide(), 2500);
-
-    //angular reevalue tous les proprietes si la vue change
-    //intant 0 : verifyemail est a 0 car ngif et httpobservable
-    //instant 1 : http et ngif resolved -> template rendered -> viewchanged event fired -> viewchild a une valeur
-    //le but c'est d'attendre par un moyen ou un autre que view child est pret avant de pouvoir l'utiliser
-    //solution possibles will inevitably involve some kinda hooker to watch changes
-    //1. dans le setter verifier quand on recoit le bon objet
-    //2. apres chaque changement de vue verifier si tooltip est attribue (ngAfterViewChecked)
-    //3. viewchildren.changes/subscribe() technique
-    //setter technique bad trigger error before --- + link
-    //cleanest sol is ngchecked go for it
+    this.verifyEmailTooltip.changes.subscribe({
+      next: (tooltips: QueryList<MatTooltip>) => {
+        tooltips?.first?.show();
+        setTimeout(() => tooltips?.first?.hide(), 3000);
+        this.cdr.detectChanges();
+      }
+    });
   }
 
   disconnect(): void {
