2fbb8f0232bf23088474975eecb0839873a3c847
Noureddine <dev.ziani@gmail.com>
2021-05-07 17:36:56 +0200

    progress bar added

--- client/src/app/features/account/pages/google-sign-page/google-sign-page.component.ts
+++ client/src/app/features/account/pages/google-sign-page/google-sign-page.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './google-sign-page.component.html',
   styleUrls: ['./google-sign-page.component.scss']
 })
-export class GoogleSignPageComponent implements OnInit {
+export class GoogleSignPageComponent implements AfterViewInit {
+  progressStep = '0%';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    //Apply loading in progress effect
+    setTimeout(() => (this.progressStep = '50%'), 300);
   }
 
+  cancelRegistration(): void {
+    //
+  }
+
+  completeRegistration(): void {
+    //Apply loading completed progress effect
+    setTimeout(() => (this.progressStep = '100%'), 300);
+  }
 }
