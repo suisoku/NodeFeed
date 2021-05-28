1bfe3c3ab1205721d9751190acfd63f4639c4968
Noureddine <dev.ziani@gmail.com>
2021-05-28 15:27:20 +0200

    testing stuff added

--- client/src/app/features/authentication/services/authentication.service.spec.ts
+++ client/src/app/features/authentication/services/authentication.service.spec.ts
@@ -1,12 +1,47 @@
 import { TestBed } from '@angular/core/testing';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
 
 import { AuthenticationService } from './authentication.service';
 
-describe('AuthenticationService', () => {
+fdescribe('AuthenticationService', () => {
   let service: AuthenticationService;
+  let router: jasmine.SpyObj<Router>; //instance mock
+  let angularFireStore: jasmine.SpyObj<AngularFirestore>; //instance mock
+  let angularFireAuth: jasmine.SpyObj<AngularFireAuth>; //instance mock
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    const routerProvider = jasmine.createSpyObj('Router', ['collection']);
+    const angularFireStoreProvider = jasmine.createSpyObj('AngularFireStore', ['collection']);
+    const angularFireAuthProvider = jasmine.createSpyObj('AngularFireAuth', [
+      'createUserWithEmailAndPassword',
+      'signInWithEmailAndPassword',
+      'signOut',
+      'signInWithPopup',
+      'sendPasswordResetEmail',
+      'user'
+    ]); //provider mock
+
+    //angularFireAuthProvider.user.and.returnValue(of([{ user: 'fakeUser' }]));
+    const fakeUser$ = of([{ user: 'fakeUser' }]);
+    angularFireAuthProvider.user.and.returnValue(fakeUser$);
+
+    TestBed.configureTestingModule({
+      providers: [
+        AuthenticationService,
+        { provide: Router, useValue: routerProvider },
+        { provide: AngularFirestore, useValue: angularFireStoreProvider },
+        { provide: AngularFireAuth, useValue: angularFireAuthProvider }
+      ]
+    });
+
+    // injected instance mocks
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+    angularFireStore = TestBed.inject(AngularFirestore) as jasmine.SpyObj<AngularFirestore>;
+    angularFireAuth = TestBed.inject(AngularFireAuth) as jasmine.SpyObj<AngularFireAuth>;
+
     service = TestBed.inject(AuthenticationService);
   });
 
--- client/src/app/features/post/services/posts.service.spec.ts
+++ client/src/app/features/post/services/posts.service.spec.ts
@@ -1,16 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import { TestBed, inject, waitForAsync } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { PostsService } from './posts.service';
 
 describe('Service: Posts', () => {
+  let serviceMock: PostsService;
+  let angularFireStoreInstanceMock: jasmine.SpyObj<AngularFirestore>;
+
   beforeEach(() => {
+    //mock and provide AngularFirestore
+    const angularFireStoreMock = jasmine.createSpyObj('AngularFireStore', ['collection']);
+
     TestBed.configureTestingModule({
-      providers: [PostsService]
+      providers: [PostsService, { provide: AngularFirestore, useValue: angularFireStoreMock }]
     });
+
+    angularFireStoreInstanceMock = TestBed.inject(AngularFirestore) as jasmine.SpyObj<AngularFirestore>;
+    serviceMock = TestBed.inject(PostsService);
   });
 
-  it('should ...', inject([PostsService], (service: PostsService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should be truthy', () => {
+    expect(serviceMock).toBeTruthy();
+  });
 });
