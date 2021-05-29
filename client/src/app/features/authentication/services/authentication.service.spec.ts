557d8d23b91712cf41e43a28779e4a32ef60b4d4
Noureddine <dev.ziani@gmail.com>
2021-05-29 16:34:56 +0200

    register method continuing testing

--- client/src/app/features/authentication/services/authentication.service.spec.ts
+++ client/src/app/features/authentication/services/authentication.service.spec.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable rxjs/finnish */
 import { TestBed } from '@angular/core/testing';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { of } from 'rxjs';
-import { FirebaseCredential } from 'src/firebase-app';
+import { FirebaseCredential, FirebaseUser } from 'src/firebase-app';
 import { CredentialsModel } from '../models/credentials.model';
 import { SignInDetailsModel } from '../models/sign-in-details.model';
 import { AuthenticationService } from './authentication.service';
@@ -21,7 +22,7 @@ fdescribe('AuthenticationService', () => {
 
     // Mock providers
     const routerProvider = jasmine.createSpyObj('Router', ['collection']);
-    const angularFireStoreProvider = jasmine.createSpyObj('AngularFireStore', ['collection']);
+    const angularFireStoreProvider = jasmine.createSpyObj('AngularFireStore', ['doc']);
     const angularFireAuthProvider = jasmine.createSpyObj(
       'AngularFireAuth',
       ['createUserWithEmailAndPassword', 'signInWithEmailAndPassword', 'signOut', 'signInWithPopup', 'sendPasswordResetEmail'],
@@ -63,7 +64,50 @@ fdescribe('AuthenticationService', () => {
   });
 
   it('calls registerUser with nominal signInDetails object', () => {
-    //
+    const signInformation: SignInDetailsModel = {
+      email: 'abc@abc.fr',
+      password: 'abc',
+      birthDay: 1,
+      birthMonth: 10,
+      birthYear: 1996,
+      gender: 'Male',
+      name: 'Nour'
+    };
+
+    const userMock = {
+      email: 'fakeUser@abc.com',
+      updateProfile: (profile: { displayName: string }) => Promise.resolve(),
+      sendEmailVerification: () => Promise.resolve()
+    } as FirebaseUser;
+    spyOn(userMock, 'updateProfile');
+
+    const userCredential = {
+      user: userMock
+    } as FirebaseCredential;
+
+    angularFireAuth.createUserWithEmailAndPassword.and.returnValue(Promise.resolve(userCredential));
+    angularFireStore.doc.and.returnValue(({
+      set: () => Promise.resolve()
+    } as unknown) as AngularFirestoreDocument<SignInDetailsModel>);
+
+    //act
+    service.registerUser(signInformation);
+
+    //evaluate
+    expect(angularFireAuth.createUserWithEmailAndPassword).toHaveBeenCalledOnceWith(
+      signInformation.email,
+      signInformation.password
+    );
+    console.log((userMock.updateProfile as any).calls.any()); //it fires corect type of result , so the problem must be async cycles.
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    expect((userMock.updateProfile as any).calls.any()).toHaveBeenCalledTimes(1);
+    // I expect that an email verification is sent
+    // I expect to have correct document
+    // Expect user doc methods being called
+
+    //2 considerations you need the whole function to exectue successfull
+    //catch exceptions  + mock everything that is left to mock
+    // async cycles to handle
   });
 
   it('calls registerUser case2', () => {
