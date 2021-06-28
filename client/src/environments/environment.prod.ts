0c49cb7b6efccc703409322257c3c951a014fe6a
Noureddine <dev.ziani@gmail.com>
2021-06-28 13:32:16 +0200

    firebase hosting setting

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCfGz_5hhfjiff-7IZIGdllF2uR8pqC4iw',
+    authDomain: 'nodefeed-ce139.firebaseapp.com',
+    projectId: 'nodefeed-ce139',
+    storageBucket: 'nodefeed-ce139.appspot.com',
+    messagingSenderId: '1099115545385',
+    appId: '1:1099115545385:web:3b475151fb027fddc22d38',
+    measurementId: 'G-DZMCMSGXPG'
+  }
 };
