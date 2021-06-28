461fb1c709ba58f615f5f7e45ff00092fa96edef
Noure <dev.ziani@gmail.com>
2021-06-28 14:40:55 +0200

    some pr test

--- client/src/app/shared/pipes/short-number.pipe.spec.ts
+++ client/src/app/shared/pipes/short-number.pipe.spec.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { ShortNumberPipe } from './short-number.pipe';
-
+// TODO: a test should be done for this pipe
 describe('Pipe: ShortNumbere', () => {
   it('create an instance', () => {
     const pipe = new ShortNumberPipe();
