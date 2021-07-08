ee6705eb606b8a4db7839b8d0831ab5a34193600
Noureddine <dev.ziani@gmail.com>
2021-07-09 01:11:02 +0200

    im bored of it

--- client/src/app/features/feed/components/create-feed-modal/create-feed-modal.component.ts
+++ client/src/app/features/feed/components/create-feed-modal/create-feed-modal.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
-import { NodefeedModel } from 'src/app/core/models/nodefeed.model';
+import { filter, switchMap, tap } from 'rxjs/operators';
 import { NodefeedService } from 'src/app/core/services/nodefeed.service';
 
 /**
@@ -14,22 +14,32 @@ import { NodefeedService } from 'src/app/core/services/nodefeed.service';
 export class CreateFeedModalComponent implements OnInit, AfterViewInit {
   nodefeedNameControl!: FormControl;
   progressStep = '0%';
+  nameExists = false;
 
   constructor(public nodefeedService: NodefeedService, private modalRef: MatDialogRef<CreateFeedModalComponent>) {}
 
   ngOnInit(): void {
-    this.nodefeedNameControl = new FormCossntrol('', { updateOn: 'blur', validators: [Validators.minLength(3)] });
-    this.nodefeedNameControl.valueChanges.subscribe((name: string) => {
-      //TODO: add validator to ban special characters
-      this.nodefeedService.getNodeFeed$(name).subscribe((nodefeed: NodefeedModel) =>)
-    });
+    this.nodefeedNameControl = new FormControl('', { updateOn: 'blur', validators: [Validators.minLength(3)] });
+    this.nodefeedNameControl.valueChanges
+      .pipe(
+        tap((value) => console.log(value)),
+        filter((nodeFeedName: string) => nodeFeedName.length >= 3),
+        switchMap((nodeFeedName) => this.nodefeedService.getNodeFeed$(nodeFeedName))
+      )
+      .subscribe((nodefeed) => {
+        this.nameExists = !!nodefeed;
+      });
   }
 
   ngAfterViewInit(): void {
     setTimeout(() => (this.progressStep = '25%'), 300);
   }
 
+  createNodefeedPage(): void {
+    this.nodefeedService.getNodeFeed$(this.nodefeedNameControl.value); //kiss dude
+  }
+
   closeModal(): void {
-    this.modalRef.close('Pizza!');
+    this.modalRef.close();
   }
 }
